e4d39fc64f7890b8887b41064c08c727237d3fa5
RifabAhamed <rifabahamed@gmail.com>
2024-02-29 15:46:58 +0530

    Edited the JS file

--- script.js
+++ script.js
@@ -1,1 +1,87 @@
-console.log("test");
+//Define html elements
+const board = document.getElementById("game-board");
+
+//Define Game variables
+const gridSize = 20;
+let snake = [{ x: 10, y: 10 }];
+let food = generateFood();
+let direction = 'right';
+
+//Draw game map, snake, food
+function draw() {
+  board.innerHTML = "";
+  drawSnake();
+  drawFood();
+}
+
+//Draw Snake
+function drawSnake() {
+  snake.forEach((segment) => {
+    const snakeElement = createGameElement("div", "snake");
+
+    setPosition(snakeElement, segment);
+    board.appendChild(snakeElement);
+  });
+}
+
+//Create a snake or food cube/div
+function createGameElement(tag, className) {
+  const element = document.createElement(tag);
+  element.className = className;
+  return element;
+}
+
+//Set the position of snake or food
+function setPosition(element, position) {
+  element.style.gridColumn = position.x;
+  element.style.gridRow = position.y;
+}
+
+
+
+//Draw Food Function
+function drawFood(){
+    const foodElement = createGameElement('div', 'food');
+    setPosition(foodElement, food);
+    board.appendChild(foodElement);
+}
+
+//Generate Food
+function generateFood(){
+    const x = Math.floor(Math.random() * gridSize) + 1;
+    const y = Math.floor(Math.random() * gridSize) + 1;
+    return {x,y};
+}
+
+//Moving the snake
+function move(){
+    const head = {...snake[0]}; //get first
+    switch (direction){
+        case 'up':
+            head.y--;
+            break;
+        case 'down':
+            head.y++;
+            break;
+        case 'left':
+            head.x--;
+            break;
+        case 'right':
+            head.x++;
+            break;
+    }
+
+    snake.unshift(head);
+
+    snake.pop(); //removes last element from array
+}
+
+//Testing draw function
+draw();
+
+//Test Moving
+setInterval(() => {
+    move();
+    draw();
+}, 200);
+
